c1c163f2ef0c7d357079990efc402132227ea5ae
Wayne Manselle <viridianforge@gmail.com>
2015-06-15 10:56:07 -0700

    Honing R script

--- LivingCosts/GenerateChoropleths.r
+++ LivingCosts/GenerateChoropleths.r
@@ -12,3 +12,14 @@ OnePOneBA <- as.numeric(sub('%','',stateData[["fmr1A1"]]))
 OnePOneBF <- as.numeric(sub('%','',stateData[["fmr1F1"]]))
 TwoPTwoBA <- as.numeric(sub('%','',stateData[["fmr2A2"]]))
 TwoPTwoBF <- as.numeric(sub('%','',stateData[["fmr2F2"]]))
+
+#Generate the Color Labellings for the Maps
+OnePOneBACol <- as.character(cut(OnePOneBA,c(0,30,50,75,100,1000),labels=c("green","yellow","orange","red","purple")))
+OnePOneBFCol <- as.character(cut(OnePOneBF,c(0,30,50,75,100,1000),labels=c("green","yellow","orange","red","purple")))
+TwoPTwoBACol <- as.character(cut(TwoPTwoBA,c(0,30,50,75,100,1000),labels=c("green","yellow","orange","red","purple")))
+TwoPTwoBFCol <- as.character(cut(TwoPTwoBF,c(0,30,50,75,100,1000),labels=c("green","yellow","orange","red","purple")))
+
+OnePOneBAMap <- map("state",fill=1, col=OnePOneBACol)
+OnePOneBFMap <- map("state",fill=1, col=OnePOneBFCol)
+TwoPTwoBAMap <- map("state",fill=1, col=TwoPTwoBACol)
+TwoPTwoBFMap <- map("state",fill=1, col=TwoPTwoBFCol)
